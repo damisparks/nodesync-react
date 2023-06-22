2fc4728c587f963244d2df8b5ed30063d7d4c511
Damilola Omifare <omifaredammy@gmail.com>
2023-06-22 12:58:17 +0100

    fix: solve linting issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import AutoImport from "unplugin-auto-import/vite";
-import react from "@vitejs/plugin-react";
-import { fileURLToPath, URL } from "url";
+import AutoImport from 'unplugin-auto-import/vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,14 +9,14 @@ export default defineConfig({
     react(),
 
     AutoImport({
-      imports: ["react"],
-      dts: "src/auto-imports.d.ts",
+      imports: ['react'],
+      dts: 'src/auto-imports.d.ts',
     }),
   ],
 
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-});
+})
